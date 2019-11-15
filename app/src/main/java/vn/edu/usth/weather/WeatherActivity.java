b2c0f0723fbc5702c4e255c05cc833f7d587761b
Đỗ Ngọc Anh <30687323+ngocin@users.noreply.github.com>
2019-11-15 11:36:57 +0700

    Pratical Work 3

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,47 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("Create", "Call Create");
+    // Create a new Fragment to be placed in the activity layout
+        ForecastFragment firstFragment = new ForecastFragment();
+    // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Start", "Call Start");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Resume ", "Call Resume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Pause", "Call Pase");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Stop", "Call Stop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Destroy", "Call Destroy");
+    }
+}
+
