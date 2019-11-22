38d12fdae6ecb03ca34b1e66b616d1970046bfe9
Đỗ Ngọc Anh <30687323+ngocin@users.noreply.github.com>
2019-11-22 22:56:48 +0700

    Pratical Work 5

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -3,46 +3,42 @@ package vn.edu.usth.weather;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-
+import androidx.fragment.app.Fragment;
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class ForecastFragment extends Fragment {
     public ForecastFragment() {
-
+        // Required empty public constructor
     }
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0xFF1ed7e8);
+        view.setBackgroundColor(0xFFE0E0FF);
 
+        /*
         //practical work 4 start here
         LinearLayout fragment_container = view.findViewById(R.id.fragment_forecast);
         fragment_container.setOrientation(LinearLayout.VERTICAL);
-
         // add the button in fragment
         ImageView weather = new ImageView(getContext());
-        weather.setImageResource(R.drawable.sun_cloud);
-
+        weather.setImageResource(R.drawable.sunny);
         TextView day = new TextView(getContext());
         day.setText("Thursday");
         day.setTextSize(34);
         day.setBackgroundColor(0xFF1ed7e8);
         day.setPadding(0, 10, 0, 10);
-
         fragment_container.addView(weather);
         fragment_container.addView(day);
+        */
 
         return view;
     }
