c02fbbbcdff7dd4f7b0013e1b5cff2a82d473501
Đỗ Ngọc Anh <30687323+ngocin@users.noreply.github.com>
2019-11-23 03:31:31 +0700

    Pratical Work 7
    Somehow, it worked

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -13,10 +15,12 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i("Create", "Call Create");
-    // Create a new Fragment to be placed in the activity layout
-        ForecastFragment firstFragment = new ForecastFragment();
-    // Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+
+        PagerAdapter adapter = new WeatherAndForecastFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
     }
     @Override
     protected void onStart() {
